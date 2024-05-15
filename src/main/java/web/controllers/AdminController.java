677313a85eb0f1f5ec552adbc666e063d3413862
noha <nohameggaiz@gmail.com>
2024-05-15 20:59:59 +0100

    first commit

---
+++ src/main/java/web/controllers/AdminController.java
@@ -0,0 +1,111 @@
+package web.controllers;
+
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import web.models.Admin;
+import web.models.Agent;
+import web.repositories.AdminRepo;
+import web.repositories.AgentRepo;
+import web.request.SignupRequest;
+import web.request.SignupRequestAgent;
+import web.service.AdminService;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/api")
+public class AdminController {
+
+    @Autowired
+    private AdminService adminserv;
+
+    @Autowired
+    AgentRepo agentRepo;
+
+    @Autowired
+    AdminRepo adminRepo;
+
+    @PostMapping
+    public ResponseEntity<?> createAgent(@RequestBody SignupRequestAgent agentRequest) throws IOException {
+        // Check if the email already exists
+        if (agentRepo.existsByEmail(agentRequest.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("Error: Email is already in use!");
+        }
+
+        // Proceed to create the agent if the email does not exist
+        boolean isCreated = adminserv.createAgent(
+                agentRequest.getNom(),
+                agentRequest.getPrenom(),
+                agentRequest.getPieceIdentite(),
+                agentRequest.getNumPieceIdentite(),
+                agentRequest.getDateNaissance(),
+                agentRequest.getAdresse(),
+                agentRequest.getEmail(),
+                agentRequest.getNumTel(),
+                agentRequest.getNumMatriculation(),
+                agentRequest.getNumPattente()
+        );
+
+        if (isCreated) {
+            return ResponseEntity.ok().body("Agent created successfully.");
+        } else {
+            return ResponseEntity.status(500).body("Error: Agent creation failed!");
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Agent>>  getAgents() {
+        return ResponseEntity.ok().body(
+                adminserv.getAgents());
+    }
+
+    @GetMapping("/agent/{uid}")
+    public ResponseEntity<?> getAgent(@PathVariable String uid) {
+        Agent agent = adminserv.getAgent(uid);
+        if (agent == null) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("Error: Agent not found!");
+        }
+        return ResponseEntity.ok(agent);
+    }
+    @GetMapping("/admins")
+    public ResponseEntity<List<Admin>> getAdmins() {
+        return ResponseEntity.ok().body(adminserv.getAdmins());
+    }
+
+
+    @PostMapping("/admin/signup")
+    public ResponseEntity<?> registerAdmin(@Valid @RequestBody SignupRequest signUpRequest) {
+        if (adminRepo.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("Error: Username is already taken!");
+        }
+
+        if (adminRepo.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("Error: Email is already in use!");
+        }
+
+        // Create new user's account & also here m
+        Admin user = new Admin(signUpRequest.getUsername(),
+                signUpRequest.getPassword(),
+                signUpRequest.getEmail());
+
+
+        adminserv.saveAdmin(user);
+
+        return ResponseEntity.ok("Admin registered successfully!");
+    }
+
+
+}
---
+++ src/main/java/web/service/AdminService.java
@@ -0,0 +1,139 @@
+package web.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.models.Admin;
+import web.models.Agent;
+import web.repositories.AdminRepo;
+import web.repositories.AgentRepo;
+
+import java.io.IOException;
+import java.security.SecureRandom;
+import java.util.Date;
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class AdminService {
+
+    @Autowired
+    private final AdminRepo adminRepo;
+
+    //private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private final AgentRepo agentRepo;
+
+
+    final String letterLower = "abcdefghijklmnopqrstuvwxyz";
+    final String letterUpper= letterLower.toUpperCase();
+    final String number = "0123456789";
+    final String caractereSpeciaux = "!@#$%&*_?':,;~°^";
+    final String passworwdCombinaison= letterLower+ letterUpper + number + caractereSpeciaux;
+    public String genererPassword() {
+
+
+        SecureRandom random = new SecureRandom();
+        String password="";
+
+        password+=letterLower.charAt(random.nextInt(letterLower.length()));
+        password+=letterUpper.charAt(random.nextInt(letterUpper.length()));
+        password+=number.charAt(random.nextInt(number.length()));
+        password+=caractereSpeciaux.charAt(random.nextInt(caractereSpeciaux.length()));
+
+        for(int i=0;i<5;i++) {
+            password+=passworwdCombinaison.charAt(random.nextInt(passworwdCombinaison.length()));
+        }
+
+        return password;
+    }
+
+    //Generer le username
+    public String genererUid(String email) {
+        Long dateoftoday =  System.currentTimeMillis();
+        String dateoftodayinms = dateoftoday.toString();
+
+        SecureRandom random = new SecureRandom();
+        String uid="";
+
+        uid+=letterUpper.charAt(random.nextInt(letterUpper.length()));
+
+        for(int i=0;i<3;i++) {
+            uid+=number.charAt(random.nextInt(number.length()));
+        }
+        uid+=dateoftodayinms;
+
+        log.info("UID of agent genarated: "+uid);
+        return uid;
+    }
+
+    public Boolean createAgent(String nom, String prenom, String pieceIdentite,
+                               String numPieceIdentite, Date dateNaissance, String adresse, String email,
+                               String numTel, String numMatriculation,
+                               String numPattente) throws IOException {
+
+        Agent agent=new Agent();
+
+        agent.setNom(nom);
+        agent.setPrenom(prenom);
+        agent.setPieceIdentite(pieceIdentite);
+        agent.setNumPieceIdentite(numPieceIdentite);
+        agent.setDateNaissance(dateNaissance);
+        agent.setAdresse(adresse);
+        agent.setEmail(email);
+        agent.setNumTel(numTel);
+        agent.setNumMatriculation(numMatriculation);
+        agent.setNumPattente(numPattente);
+        String uid = this.genererUid(email);
+        agent.setUsername(uid);
+
+
+        String pass=this.genererPassword();
+        System.out.println("Password of agent "+pass);
+        log.info("Password of agent "+pass);
+        //we need to encode it before setting it to agent here
+        agent.setPassword(pass);
+
+        agentRepo.save(agent);
+
+
+        return  true;
+    }
+
+    public List<Agent> getAgents() {
+        log.info("Fetching all agents by admin");
+        return  agentRepo.findAll();
+    }
+
+    public Agent getAgent(String uid) {
+        log.info("Fetching agent by the admin {}", uid);
+        return agentRepo.findByUsername(uid);
+    }
+
+    public List<Admin> getAdmins() {
+        log.info("Fetching all Admins by admin");
+
+        return adminRepo.findAll();
+    }
+    public Admin saveAdmin(Admin admin) {
+        log.info("Saving new admin {} to the database", admin.getUsername());
+        //also here need to encode it later
+        admin.setPassword(admin.getPassword());
+        return adminRepo.save(admin);
+    }
+    public Admin checkAdminExists(String uid){
+        Admin admin1 = adminRepo.findByUsername(uid);
+
+        if (admin1 != null) return admin1;
+        else return null;
+    }
+
+
+
+}
